fc381bdf8ad1f57d10e955e25f77f9de7b171507
7788wangzi <xueqijie123@163.com>
2019-05-16 12:56:14 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSIEnumberablevsIEnumerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> oyears = new List<int>();
+            oyears.Add(2009);
+            oyears.Add(2010);
+            oyears.Add(2011);
+            oyears.Add(2012);
+            oyears.Add(2013);
+
+            // IEnumerable which could use foreach to loop access all items.
+            IEnumerable<int> ienum = (IEnumerable<int>)oyears;
+            foreach (int i in ienum)
+            {
+                Console.WriteLine(i);
+            }
+
+            // IEnumberator which could use while to loop access all items.
+            IEnumerator<int> ienumerator = oyears.GetEnumerator();
+            while(ienumerator.MoveNext())
+            {
+                Console.WriteLine(ienumerator.Current);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
